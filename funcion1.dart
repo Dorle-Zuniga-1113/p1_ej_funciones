cde8f4cb67941aea12bc988a04691a75e19c56d3
Dorle-Zuniga-1113 <164533980+Dorle-Zuniga-1113@users.noreply.github.com>
2025-02-07 16:47:40 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,21 @@
+// funcion que no devuelve valor
+//funcion saludar
+void saludar(){
+  print("hola corazon");
+}
+void escribeNombre(){
+  print("dorle Zuñiga");
+}
+
+
+
+void main(){
+  // Dorle Zuñiga mat.22308051281113
+  //llamar a la funcion saludar
+  print("Dorle Zuñiga mat.22308051281113"); 
+  print("llamando a la funcion saludar");
+  saludar();
+  print("llamando a la funcion escribeNombre");
+  escribeNombre();
+
+}
