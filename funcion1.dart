7bc7951a9f29614f301912a29689bcc59352d6ea
Dorle-Zuniga-1113 <164533980+Dorle-Zuniga-1113@users.noreply.github.com>
2025-02-10 18:04:49 +0000

    ejemplo funcion rama

--- funcion1.dart
+++ funcion1.dart
@@ -16,10 +16,9 @@ void main(){
   // Dorle Zuñiga mat.22308051281113
   //llamar a la funcion saludar
 
-
   print("Dorle Zuñiga mat.22308051281113"); 
   print("llamando a la funcion saludar");
-  
+
   saludar();
   print("llamando a la funcion escribeNombre");
   escribeNombre();
---
+++ funcion2.dart
@@ -0,0 +1,14 @@
+void suma(int a, int b) {
+  print('la suma es:${a + b}');
+}
+void resta(int a, int b) {
+  print('la resta es:${a - b}');
+}
+//funcion main
+void main() {
+  print('Dorle zuñiga 22308051281113');
+  print('aqui esta el resultado de la suma y resta de 5 y 5');
+
+  suma(5, 5);
+  resta(5, 5);
+}
