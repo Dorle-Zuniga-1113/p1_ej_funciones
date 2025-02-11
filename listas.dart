a095ab7b9cff4cc38ebbff62c1b069f4a7285863
Dorle-Zuniga-1113 <164533980+Dorle-Zuniga-1113@users.noreply.github.com>
2025-02-11 20:00:58 +0000

    lista1

---
+++ listas.dart
@@ -0,0 +1,23 @@
+void main(){
+  // Dorle Zuñiga mat.22308051281113
+  print("Dorle Zuñiga mat.22308051281113"); 
+ List<int> numeros = [1,2,3,4,5,6,7,8,9,10];
+ print(numeros);
+ print(numeros[0]);
+ //listar elementos de la lista ciclo for
+ for(int i = 0; i < numeros.length; i++){
+   print(numeros[i]);
+ }
+//lista tipo double con 5 elementos de estaturas
+ List<double> estaturas = [1.70,1.80,1.60,1.90,1.75];
+//lista de 5 nombres de personas
+  List<String> nombres = ["francisco","arturito","juan","lupita","kass"];
+  // imprimir lista y estaturas y nombres
+  for (int i = 0; i < estaturas.length; i++){
+    print('nombre: ${nombres[i]}"Estatura: ${estaturas[i]}');
+  } 
+ }
+
+
+
+
